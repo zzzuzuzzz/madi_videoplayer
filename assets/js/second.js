809ee46f6abd25db2849f3369ac6a53d7b346627
Мезенцев Евгений Александрович <mezencev_e_qschool@qsoft.ru>
2024-09-11 00:51:01 +0300

    Сделал первую версию, но в процессе переписывания

---
+++ assets/js/second.js
@@ -0,0 +1,61 @@
+class DB 
+{
+    all()
+    {
+        let xhr = new XMLHttpRequest();
+
+        xhr.open('GET', '/assets/DB.txt');
+        xhr.send();
+        xhr.onload = function() {
+            if (xhr.status != 200) {
+                console.log(`Ошибка ${xhr.status}: ${xhr.statusText}`);
+            } else {
+                let array = xhr.response;
+                let div = document.getElementById('secondBlock')
+
+                array.split(';').forEach(element => {
+                    let result = element.split('-/-');
+
+                    let mainDiv = document.createElement('div')
+                    mainDiv.className = 'col-sm-8 mb-2 w-75'
+                    mainDiv.dataset.id = result[0]
+
+                    let a = document.createElement('div')
+                    a.href = '/pages/detail.html?' + result[0]
+                    a.className = 'card'
+                    
+                    let img = document.createElement('img')
+                    img.src = '/assets/images/' + result[3] + '.png'
+                    img.className = 'card-img-top'
+                    img.alt = 'Видео превью'
+
+                    let bodyDiv = document.createElement('div')
+                    bodyDiv.className = 'card-body'
+
+                    let h = document.createElement('h5')
+                    h.className = 'card-title'
+                    h.textContent = result[1]
+
+                    let p = document.createElement('p')
+                    p.className = 'card-text'
+                    p.textContent = result[2]
+
+                    bodyDiv.append(h)
+                    bodyDiv.append(p)
+
+                    a.append(img)
+                    a.append(bodyDiv)
+
+                    mainDiv.append(a)
+
+                    div.append(mainDiv)
+                });
+            }
+        };
+    }
+};
+
+
+let dataBase = new DB;
+dataBase.all()
+
